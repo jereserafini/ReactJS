f45a0800aaa4e088ef5b13025c63aef5958f8563
jereserafini <jerefernandez98@gmail.com>
2021-12-28 20:21:24 -0300

    Agregado de boton 'agregar carrito'

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -5,6 +5,7 @@ import Button from 'react-bootstrap/Button';
 const ItemCount = ({min, max}) => {
 
     const [contador, setContador] = useState(min)
+    const [show, setShow] = useState(false)
 
     const sumaContador = () =>{
 
@@ -18,13 +19,24 @@ const ItemCount = ({min, max}) => {
 
     }
 
+    const mostrar = () => {
+        setShow(!show)
+    }
+
     return (
 
         <div className='text-center'>
             <h2>{contador}</h2>
             <Button className='mx-2' variant="outline-dark" onClick={restaContador}>-</Button>
             <Button className='mx-2' variant="outline-dark" onClick={sumaContador}>+</Button>
-        </div>
+            <br/>
+            <Button className='mx-2 my-2' variant="outline-dark" onClick={mostrar}>Agregar Carrito</Button>
+
+            {
+            show && <h3>Usted pidio {contador} productos</h3>
+            }
+
+        </div>        
 
 
     )
